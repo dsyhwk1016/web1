c98b287e3353b013ebdb4877fe89fbb18c935494
dsyhwk1016 <73643088+dsyhwk1016@users.noreply.github.com>
2020-11-05 00:36:01 +0900

    Add files via upload

--- Color.js
+++ Color.js
@@ -1,39 +1,47 @@
 var Body = {
   setColor : function(color){
-    $('body').css('color', color)
+    $('body').css('color', color);
   },
   setBackgroundColor : function(color){
-    $('body').css('background-color', color)
+    $('body').css('background-color', color);
   }
 }
 
 var Links = {
   setColor : function(color){
-    $('a').css('color', color)
+    $('a').css('color', color);
+  }
+}
+
+var Button = {
+  setColor : function(color){
+    $('.button').css('color', color);
+  },
+  setBackgroundColor : function(color){
+    $('.button').css('background-color', color);
   }
 }
 
 function changeMode(self){
   if(self.value === 'night'){
-    Body.setColor('white')
-    Body.setBackgroundColor('black')
-    Links.setColor('yellow')
-    self.value = 'day'
+    Body.setColor('white');
+    Body.setBackgroundColor('black');
+    Links.setColor('yellow');
+    self.value = 'day';
   } else{
-    Body.setColor('black')
-    Body.setBackgroundColor('white')
-    Links.setColor('blue')
-    self.value = 'night'
+    Body.setColor('black');
+    Body.setBackgroundColor('white');
+    Links.setColor('blue');
+    self.value = 'night';
   }
 }
 
-function changeOnMouse(){
-  Body.setBackgroundColor('gray')
+function changeOnMouse(self){
+  Button.setColor('white');
+  Button.setBackgroundColor('gray');
 }
 
 function changeLeaveMouse(self){
-  if(self.value === 'night')
-    Body.setBackgroundColor('white')
-  else
-    Body.setBackgroundColor('black')
+  Button.setColor('black');
+  Button.setBackgroundColor('#FEFEFE');
 }
